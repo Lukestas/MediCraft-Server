32d25c6ad5ddb12e8a8ea422e37f3ad882dffaf2
Lukestas <julianenriqueluquelopez@gmail.com>
2026-03-30 09:04:10 -0600

    feat: connect to mongodb in app entry point

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
 import { app } from './server/server.js';
 import { PORT } from './config/env.js';
+import { MongoDBConnection } from './config/mongodb.js';
+
+MongoDBConnection();
 
 app.listen(PORT, () => {
   console.log(`Server running in http://localhost:${PORT}`);
