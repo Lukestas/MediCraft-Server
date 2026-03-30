9fac88050f2214d3da1e8af9199a0a9d64c57546
Lukestas <julianenriqueluquelopez@gmail.com>
2026-03-30 11:35:58 -0600

    feat: add centralized routes entry point

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import { app } from './server/server.js';
 import { PORT } from './config/env.js';
 import { startDBConnection } from './config/mongodb.js';
+import { routes } from './routes/routes.js';
 
 startDBConnection();
+
+app.use('/api', routes);
 
 app.listen(PORT, () => {
   console.log(`Server running in http://localhost:${PORT}`);
