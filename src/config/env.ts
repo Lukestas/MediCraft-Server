a569bd51c173d8e87c6a8b98beb5dfcff0e720ab
Lukestas <julianenriqueluquelopez@gmail.com>
2026-03-30 08:58:11 -0600

    chore: add environment variable for mongodb connection

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,3 +1,5 @@
 process.loadEnvFile();
 export const PORT = process.env.PORT ?? 3001;
 export const ACCEPTED_ORIGINS = process.env.ACCEPTED_ORIGINS?.split(',') ?? [];
+export const MONGODB_API = process.env.MONGODB_API ?? '';
+
