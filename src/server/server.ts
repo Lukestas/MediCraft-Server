51e5e3a85b7f77ed99dddd9edda741d0b006da3a
Lukestas <julianenriqueluquelopez@gmail.com>
2026-03-29 22:24:15 -0600

    feat: integrate cors middleware into server file

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,10 +1,12 @@
 import express, { type Application } from 'express';
 import morgan from 'morgan';
+import { corsMiddleware } from '../middleware/cors.js';
 
 export const app: Application = express();
 
 app.use(express.json());
 app.use(morgan('dev'));
+app.use(corsMiddleware());
 app.get('/', (_req, res) => {
   return res.status(200).json({ message: 'Server Health' });
 });
