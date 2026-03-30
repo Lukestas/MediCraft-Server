458680c7bc8f42ab556f9d4dc352a58ec286b91a
Lukestas <julianenriqueluquelopez@gmail.com>
2026-03-30 09:44:17 -0600

    refactor: apply Singleton Pattern to mongodb connection

--- src/config/mongodb.ts
+++ src/config/mongodb.ts
@@ -1,12 +1,36 @@
 import mongoose from 'mongoose';
 import { MONGODB_API } from './env.js';
 
-export const MongoDBConnection = async () => {
-  try {
-    await mongoose.connect(MONGODB_API);
-    console.log('Data base connected');
-  } catch (error) {
-    console.log(`Error connecting data base: -> ${error}`);
-    process.exit(1);
+class MongoDB {
+  private static instance: MongoDB;
+  private isConnected: boolean = false;
+
+  private constructor() {}
+
+  public static getInstance(): MongoDB {
+    if (!MongoDB.instance) {
+      MongoDB.instance = new MongoDB();
+    }
+    return MongoDB.instance;
   }
+
+  public async connect(): Promise<void> {
+    if (this.isConnected) {
+      console.log('MongoDB already connected');
+      return;
+    }
+    try {
+      await mongoose.connect(MONGODB_API);
+      this.isConnected = true;
+      console.log('Database connected');
+    } catch (error) {
+      console.log(`Error connecting database: -> ${error}`);
+      process.exit(1);
+    }
+  }
+}
+
+export const startDBConnection = async () => {
+  const db = MongoDB.getInstance();
+  await db.connect();
 };
