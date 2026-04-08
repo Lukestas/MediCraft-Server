83e89f23ec31ff74b2836394030d0a60f9ced25f
Lukestas <julianenriqueluquelopez@gmail.com>
2026-04-08 12:07:43 -0600

    refactor: change method name in endpoint to get an admin

--- src/routes/admin.routes.ts
+++ src/routes/admin.routes.ts
@@ -3,7 +3,7 @@ import { AdminController } from '../controllers/admin.controller.js';
 
 export const adminRoutes: Router = Router();
 
-adminRoutes.get('/:id', AdminController.getId);
+adminRoutes.get('/:id', AdminController.getById);
 adminRoutes.post('/', AdminController.create);
 adminRoutes.delete('/:id', AdminController.delete);
 adminRoutes.put('/:id', AdminController.update);
