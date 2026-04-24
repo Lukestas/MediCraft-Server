299ebae566ddbc932a3c513ce72520edb0ffd706
Lukestas <julianenriqueluquelopez@gmail.com>
2026-04-24 12:54:25 -0600

    feat: add doctor routes with CRUD endpoints

--- src/routes/doctor.routes.ts
+++ src/routes/doctor.routes.ts
@@ -1,7 +1,10 @@
 import { Router } from 'express';
+import { DoctorController } from '../controllers/doctor.controller.js';
 
 export const doctorRoutes: Router = Router();
 
-doctorRoutes.get('/', (_req, res) => {
-  return res.status(200).json({ message: 'You are in Doctors' });
-});
+doctorRoutes.get('/:id', DoctorController.getById);
+doctorRoutes.get('/', DoctorController.getAll);
+doctorRoutes.post('/', DoctorController.create);
+doctorRoutes.delete('/:id', DoctorController.delete);
+doctorRoutes.put('/:id', DoctorController.update);
