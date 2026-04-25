d2cf6f61057ea97f20e1b2e42a77088d9e1d61c6
Lukestas <julianenriqueluquelopez@gmail.com>
2026-04-25 16:57:17 -0600

    feat: add user services implements user service interface operations

---
+++ src/services/user.service.ts
@@ -0,0 +1,43 @@
+import type {
+  IUser,
+  IDoctor,
+  IUserRepository,
+  IUserService,
+} from '../types/user.js';
+
+export class UserService implements IUserService {
+  private userRepository: IUserRepository;
+  constructor(userRepository: IUserRepository) {
+    this.userRepository = userRepository;
+  }
+
+  async createUser(user: IUser): Promise<IUser | IDoctor> {
+    return this.userRepository.create(user);
+  }
+
+  async findUser(type: string): Promise<IUser[] | IDoctor[] | null> {
+    return this.userRepository.find(type);
+  }
+
+  async findUserById(id: string): Promise<IUser | IDoctor | null> {
+    return this.userRepository.findById(id);
+  }
+
+  async findDNI(DNI: number): Promise<IUser | IDoctor | null> {
+    return this.userRepository.findDNI(DNI);
+  }
+
+  async findEmail(email: string): Promise<IUser | IDoctor | null> {
+    return this.userRepository.findEmail(email);
+  }
+
+  async updateUser(
+    id: string,
+    user: Partial<IUser>,
+  ): Promise<IUser | IDoctor | null> {
+    return this.userRepository.update(id, user);
+  }
+  async deleteUser(id: string): Promise<boolean> {
+    return this.userRepository.delete(id);
+  }
+}
