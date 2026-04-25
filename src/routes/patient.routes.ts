d48bc372602578ee1627943c9cc194ca7dfe66e2
Lukestas <julianenriqueluquelopez@gmail.com>
2026-04-24 23:05:58 -0600

    fix: add root path to patient routes

--- src/routes/patient.routes.ts
+++ src/routes/patient.routes.ts
@@ -3,8 +3,8 @@ import { PatientController } from '../controllers/patient.controller.js';
 
 export const patientRoutes: Router = Router();
 
-patientRoutes.post('', PatientController.create);
-patientRoutes.get('', PatientController.getAll);
+patientRoutes.post('/', PatientController.create);
+patientRoutes.get('/', PatientController.getAll);
 patientRoutes.put('/:id', PatientController.update);
 patientRoutes.get('/:id', PatientController.getById);
 patientRoutes.delete('/:id', PatientController.delete);
