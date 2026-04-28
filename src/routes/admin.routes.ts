3793daed0027aee1e761b6c048689c0e5a3f7edd
Lukestas <julianenriqueluquelopez@gmail.com>
2026-04-28 17:55:04 -0600

    feat: delete admin routes

--- src/routes/admin.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { AdminController } from '../controllers/admin.controller.js';
-
-export const adminRoutes: Router = Router();
-
-adminRoutes.get('/:id', AdminController.getById);
-adminRoutes.post('/', AdminController.create);
-adminRoutes.delete('/:id', AdminController.delete);
-adminRoutes.put('/:id', AdminController.update);
