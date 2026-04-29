05fe25a17307eeda5f63870b52224798e372fa39
Lukestas <julianenriqueluquelopez@gmail.com>
2026-04-28 18:24:24 -0600

    feat: add appointment validation as middleware in post and put routes

--- src/routes/appointment.routes.ts
+++ src/routes/appointment.routes.ts
@@ -1,10 +1,19 @@
 import { Router } from 'express';
 import { AppointmentController } from '../controllers/appointment.controller.js';
+import { AppointmentDataValidator } from '../middleware/appointment.data.validator.js';
 
 export const appointmentRoutes: Router = Router();
 
+appointmentRoutes.post(
+  '/',
+  AppointmentDataValidator.createAppointmentData,
+  AppointmentController.create,
+);
 appointmentRoutes.get('/', AppointmentController.getAll);
 appointmentRoutes.get('/:id', AppointmentController.getById);
-appointmentRoutes.post('/', AppointmentController.create);
+appointmentRoutes.put(
+  '/:id',
+  AppointmentDataValidator.updateAppointmentData,
+  AppointmentController.update,
+);
 appointmentRoutes.delete('/:id', AppointmentController.delete);
-appointmentRoutes.put('/:id', AppointmentController.update);
