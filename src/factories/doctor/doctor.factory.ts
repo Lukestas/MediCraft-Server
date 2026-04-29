b7b816cc2f83fee976b8eb15f1154651223a4f8b
Lukestas <julianenriqueluquelopez@gmail.com>
2026-04-28 18:10:11 -0600

    feat: add null result if type is different of cardiology or dermatology

--- src/factories/doctor/doctor.factory.ts
+++ src/factories/doctor/doctor.factory.ts
@@ -3,14 +3,14 @@ import { DermatologyFactory } from './dermatology.factory.js';
 import type { IDoctorFactory } from './doctor-factory.interface.js';
 
 export class DoctorFactory {
-  static getFactory(type: string): IDoctorFactory {
+  static getFactory(type: string): IDoctorFactory | null {
     switch (type) {
       case 'cardiology':
         return new CardiologyFactory();
       case 'dermatology':
         return new DermatologyFactory();
       default:
-        throw new Error('Invalid specialty');
+        return null;
     }
   }
 }
