dc39aaebae4c4060bf16e81675b4d10a5bab8997
Lukestas <julianenriqueluquelopez@gmail.com>
2026-04-28 18:09:24 -0600

    feat: add null result if type is different of patient or admint

--- src/factories/user/user.factory.ts
+++ src/factories/user/user.factory.ts
@@ -3,7 +3,10 @@ import type { IUser } from '../../types/user.js';
 type CreateUserData = Omit<IUser, 'role' | 'id'>;
 
 export class UserFactory {
-  static createUser(type: 'patient' | 'admin', data: CreateUserData): IUser {
+  static createUser(
+    type: 'patient' | 'admin',
+    data: CreateUserData,
+  ): IUser | null {
     const baseUser: IUser = {
       ...data,
       role: type,
@@ -15,7 +18,7 @@ export class UserFactory {
       case 'admin':
         return baseUser;
       default:
-        throw new Error('Invalid user type');
+        return null;
     }
   }
 }
