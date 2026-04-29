e88d6109371635f9750bacedb3052c676bced70e
Lukestas <julianenriqueluquelopez@gmail.com>
2026-04-28 18:26:11 -0600

    feat: add user crud endpoint, post and put using user data validator

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,7 +1,11 @@
 import { Router } from 'express';
 import { UserController } from '../controllers/user.controller.js';
+import { UserDataValidator } from '../middleware/user.data.validator.js';
 
 export const userRoutes: Router = Router();
 
 userRoutes.get('/', UserController.getAll);
-userRoutes.post('/', UserController.create);
+userRoutes.get('/:id', UserController.getById);
+userRoutes.post('/', UserDataValidator.createUserData, UserController.create);
+userRoutes.put('/:id', UserDataValidator.updateUserData, UserController.update);
+userRoutes.delete('/:id', UserController.delete);
